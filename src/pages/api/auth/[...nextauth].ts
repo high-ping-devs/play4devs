cfa46734b9cedbc7f46529e3154c9547b8ee6b07
joevtap <joelvitortorres@gmail.com>
2023-06-13 23:31:05 -0300

    MOMENTO EPIFANIA

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,32 +7,32 @@ export const authOptions = {
         SpotifyProvider({
             clientId: process.env.SPOTIFY_CLIENT_ID!,
             clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
-            checks: ['none'],
-            allowDangerousEmailAccountLinking: true,
+            // checks: ['none'],
+            // allowDangerousEmailAccountLinking: true,
         }),
     ],
-    // callbacks: {
-    //     // @ts-ignore
-    //     async jwt({ token, account, profile }) {
-    //         // Persist the OAuth access_token and or the user id to the token right after signin
-    //         if (account) {
-    //             token.accessToken = account.access_token
-    //             token.refreshToken = account.refresh_token
-    //             token.id = profile.id
-    //         }
-    //         return token
-    //     },
-    //     // @ts-ignore
-    //     async session({ session, token, user }) {
-    //         // Send properties to the client, like an access_token and user id from a provider.
-    //         session.accessToken = token.accessToken
-    //         session.refreshToken = token.refreshToken
-    //         session.user.id = token.id
+    callbacks: {
+        // @ts-ignore
+        async jwt({ token, account, profile }) {
+            // Persist the OAuth access_token and or the user id to the token right after signin
+            if (account) {
+                token.accessToken = account.access_token
+                token.refreshToken = account.refresh_token
+                token.id = profile.id
+            }
+            return token
+        },
+        // @ts-ignore
+        async session({ session, token, user }) {
+            // Send properties to the client, like an access_token and user id from a provider.
+            session.accessToken = token.accessToken
+            session.refreshToken = token.refreshToken
+            session.user.id = token.id
 
-    //         return session
-    //     }
+            return session
+        }
 
-    // }
+    }
 }
 
 // @ts-ignore
