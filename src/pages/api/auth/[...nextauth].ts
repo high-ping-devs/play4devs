389ce2d40dae2f1d26419fe517d9d9f837a498f6
joevtap <joelvitortorres@gmail.com>
2023-06-13 22:36:11 -0300

    trying disabling checks for spotify provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,7 +7,7 @@ export const authOptions = {
         SpotifyProvider({
             clientId: process.env.SPOTIFY_CLIENT_ID!,
             clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
-
+            checks: ['none']
         }),
     ],
     callbacks: {
