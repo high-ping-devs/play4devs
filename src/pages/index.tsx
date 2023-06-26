6a48ab6d8f8b53c5e849433efa9589eb1801735f
GianlucaBorges <91564925+GianlucaBorges@users.noreply.github.com>
2023-06-25 21:43:34 -0300

    Design da página principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,9 @@ import {
   useSpotifyProfile,
 } from "@/hooks/spotify";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import Login from "./login";
+import PlaylistPreview from "@/components/PlaylistPreview";
+import Header from "@/components/Header";
 
 export default function Component() {
   const [usersPlaylists, usersPlaylistsError] = useSpotifyUserPlaylists(1, 0);
@@ -24,12 +27,57 @@ export default function Component() {
   if (error) return <div>{error.message}</div>;
 
   if (user) {
+    const playlists = [
+      {
+        img: "https://images.unsplash.com/photo-1546527868-ccb7ee7dfa6a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        username: "User 1",
+        name: "Playlist 1",
+      },
+      {
+        img: "https://images.unsplash.com/photo-1546527868-ccb7ee7dfa6a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        username: "User 2",
+        name: "Playlist 2",
+      },
+      {
+        img: "https://images.unsplash.com/photo-1546527868-ccb7ee7dfa6a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        username: "User 3",
+        name: "Playlist 3",
+      },
+      {
+        img: "https://images.unsplash.com/photo-1546527868-ccb7ee7dfa6a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        username: "User 4",
+        name: "Playlist 4",
+      },
+    ];
     return (
       <>
-        <h1>Signed in as {user.name}</h1>
+        <Header />
+        <h1 className="flex justify-between px-3 mb-5">
+          <span className="text-lg font-semibold self-center">
+            Descubra playlists dos seus colegas ao redor do mundo
+          </span>
+          <img width={88} height={1} src="/assets/logo.svg" alt="Logo" />
+        </h1>
 
-        <p>{user.email}</p>
+        <div className="border-2 rounded">Busca</div>
 
+        <div>Estilos</div>
+
+        <ul className="flex flex-wrap gap-5 mx-5 justify-center mobile:justify-normal">
+          {playlists &&
+            playlists.map((playlist, index) => (
+              <li key={index}>
+                <PlaylistPreview
+                  imgUrl={playlist.img}
+                  imgHeight={index % 2 === 0 ? "h-40" : "h-44"}
+                  username={playlist.username}
+                  playlistName={playlist.name}
+                />
+              </li>
+            ))}
+        </ul>
+
+        {/* Usado para testes */}
         {spotifyProfile && (
           <img
             src={
@@ -63,6 +111,12 @@ export default function Component() {
           <summary>Playlist</summary>
           <pre>{!playlistError && JSON.stringify(playlist, null, 2)}</pre>
         </details>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Login />
       </>
     );
   }
