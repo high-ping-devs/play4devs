4cef33b189fff5b1aadfef8fe718edf9dc62fce9
GianlucaBorges <91564925+GianlucaBorges@users.noreply.github.com>
2023-06-25 21:44:08 -0300

    Perfil do usuário

---
+++ src/pages/profile.tsx
@@ -0,0 +1,36 @@
+import Header from '@/components/Header'
+import React from 'react'
+import UserProfileImage from '@/components/UserProfileImage';
+import LikeButton from '@/components/LikeButton';
+import { useSpotifyTracks, useSpotifyUserPlaylists } from '@/hooks/spotify';
+
+export default function profile(){
+  const [playlist, playlistError] = useSpotifyUserPlaylists(1, 0);
+
+  let dadosPlaylist = null
+  if (playlist) {
+    dadosPlaylist = playlist.items[0]
+  }
+    return (
+      <div>
+        <Header />
+        {playlist && (
+          <img src={dadosPlaylist?.images[0].url} alt="Capa da Playlist" className='w-full h-32 border-y-2' />
+        )}
+        <UserProfileImage  width={88} transform='-translate-y-12' margin='ml-5'/>
+        <div className='flex justify-between mx-5'>
+          <div className='flex'>
+            <img src="/assets/spotify.svg" alt="Logo do spotify"  className='w-6'/>
+            <span className='self-center text-base font-semibold'>Nome da playlist</span>
+          </div>
+          <LikeButton transform='-translate-y-12' />
+        </div>
+        <div className='flex flex-col mx-5'>
+          <span className='font-semibold text-base'>{dadosPlaylist?.name}</span>
+          <span className='text-gray text-sm mb-4'>{dadosPlaylist?.tracks.total} faixas</span>
+          <a href={dadosPlaylist?.external_urls.spotify} target='_blank' className='flex gap-1 text-sm hover:underline'>Ouvir no Spotify <img src="/assets/Redirect.svg" alt="Redirecionar" /></a>
+        </div>
+        
+      </div>
+    )  
+}
