80f83e93763149304cc22a5d9836803020a278ee
Gianluca <gianluca15.gb@gmail.com>
2023-07-02 15:12:28 -0300

    Corrigindo o design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 import UserProfileImage from "./UserProfileImage";
+import { Roboto_Mono } from "next/font/google";
+const roboto = Roboto_Mono({ subsets: ["latin"] });
 
 export default function Header() {
-  const { user } = useUser();
+const { user } = useUser();
 
   if (user) {
     return (
       <header className="flex justify-between py-3 px-3 mb-5 border-b-2">
-        <span className="self-center font-medium">{user.name}</span>
+        <a href="/" className="self-center font-medium font-sans" style={{ fontFamily: roboto.style.fontFamily }}>play4devs</a>
         <a href="/profile">
           <UserProfileImage width={50} />
         </a>
