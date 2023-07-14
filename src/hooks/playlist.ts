85cb0460137ed6d6b143c3c4f45785b6ee0565f6
CaiqueBrandani <caique.brandani@gmail.com>
2023-07-14 13:02:38 -0300

    add playlist hooks

--- src/hooks/playlist.ts
+++ src/hooks/playlist.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function usePlaylistSearch() {
     const [playlists, setPlaylists] = useState();
@@ -30,4 +30,99 @@ export function usePlaylistSearch() {
     // }, [searchQuery])
 
     return [search, playlists, error] as [typeof search, typeof playlists, typeof error];
+}
+
+export function useGetPlaylistByUserId(userId: string) {
+    const [playlist, setPlaylist] = useState();
+    const [error, setError] = useState<null | string>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch(`/api/playlist/by-user-id/${userId}`);
+
+                if (res.ok) {
+                    setPlaylist(await res.json());
+                    setError(undefined);
+                } else {
+                    setError(`Error: ${res.status} - ${await res.text()}`);
+                }
+            } catch (error: any) {
+                setError(`Error: ${error.message}`);
+            }
+        };
+
+        fetchData();
+
+    }, [userId])
+
+    // USAGE:
+
+    // const [playlistByUserId, playlistByUserIdError] = useGetPlaylistByUserId("64b14b2b64a36830a8b039d4")
+
+
+    return [playlist, error] as [typeof playlist, typeof error]
+}
+
+export function useGetAllPlaylists(limit: number, offset: number) {
+    const [playlist, setPlaylist] = useState();
+    const [error, setError] = useState<null | string>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch(`/api/playlist/all?limit=${limit}&offset=${offset}`);
+
+                if (res.ok) {
+                    setPlaylist(await res.json());
+                    setError(undefined);
+                } else {
+                    setError(`Error: ${res.status} - ${await res.text()}`);
+                }
+            } catch (error: any) {
+                setError(`Error: ${error.message}`);
+            }
+        };
+
+        fetchData();
+
+    }, [])
+
+    // USAGE:
+
+    // const [playlists, playlistsError] = useGetAllPlaylists(15, 0)
+
+    return [playlist, error] as [typeof playlist, typeof error]
+
+}
+
+export function useGetPlaylistById(id: string) {
+    const [playlist, setPlaylist] = useState();
+    const [error, setError] = useState<null | string>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch(`/api/playlist/${id}`);
+
+                if (res.ok) {
+                    setPlaylist(await res.json());
+                    setError(undefined);
+                } else {
+                    setError(`Error: ${res.status} - ${await res.text()}`);
+                }
+            } catch (error: any) {
+                setError(`Error: ${error.message}`);
+            }
+        };
+
+        fetchData();
+
+    }, [id])
+
+    // USAGE:
+
+    // const [playlistById, playlistByIdError] = useGetPlaylistById("64b14b3f64a36830a8b039d8")
+
+    return [playlist, error] as [typeof playlist, typeof error]
 }
